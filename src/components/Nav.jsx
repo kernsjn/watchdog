23d4f7a28fd8392c624a1539aabe34657ea66a1e
Nikki Kerns <nkernns@admorgan.com>
2020-01-13 14:06:48 -0500

    styling error fixed on dashboard

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,24 +4,22 @@ import watchdog from '../assets/watchdog.png'
 
 const Nav = () => {
   return (
-    <Router>
-      <nav>
-        <header className="navigation-bar">
-          {' '}
-          <Link to="/">
-            <img id="logo" src={watchdog} />{' '}
-          </Link>
-        </header>
-        <ul>
-          <li className="nav">
-            <Link to="./punchlist">Punchlist</Link>
-          </li>
-          <li className="nav">
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </nav>
-    </Router>
+    <nav>
+      <header className="navigation-bar">
+        {' '}
+        <Link to="/">
+          <img id="logo" src={watchdog} />{' '}
+        </Link>
+      </header>
+      <ul>
+        <li className="nav">
+          <Link to="/punchlist">Punchlist</Link>
+        </li>
+        <li className="nav">
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
