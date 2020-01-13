3ef7a70453e84ca696044b720521b49b4dab4c70
Nikki Kerns <nkernns@admorgan.com>
2020-01-13 16:41:01 -0500

    revised the building component

--- src/components/Building.jsx
+++ src/components/Building.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 
-const Building = () => {
+const Building = props => {
   return (
     <>
-      <div>
-        <section className="dropdown-section">
-          <p className="labels-2">Building Name / Number</p>
-          <select className="building">
-            <option value="building-1">Building 1</option>
-            <option value="building-2">Building 2</option>
-            <option value="building-3">Building 3</option>
-            <option value="building-4">Building 4</option>
-          </select>
-        </section>
-      </div>
+      <main>
+        <div>
+          <section className="dropdown-section">
+            <p className="labels">Building Name / Number</p>
+            <select className="building">
+              <option value={props.buildingName}></option>
+            </select>
+          </section>
+        </div>
+      </main>
     </>
   )
 }
